2e3f57a79b5f8bfe9816eadc3a891db5d2b96cce
Victor Hugo <104472494+victorhsds@users.noreply.github.com>
2023-10-29 12:56:40 -0300

    Add files via upload

---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import {useState} from 'react';
+
+import SearchManufacturer from "./SearchManufacturer"
+
+const SearchBar = () => {
+
+    const [manufacturer, setManufacturer] = useState('');
+
+
+    const handleSearch = () => {}
+
+  return (
+    <form className="searchbar" onSubmit=
+    {handleSearch}>
+            <div className="searchbar__item"></div>
+                <SearchManufacturer
+                manufacturer={manufacturer}
+                setManufacturer={setManufacturer}
+                />
+    </form>
+  )
+}
+
+export default SearchBar
---
+++ types/index.ts
@@ -0,0 +1,30 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    containerStyles?: string;
+    handleClick?:
+    MouseEventHandler<HTMLButtonElement>;
+    btnType? :"button" | "submit" | "reset" | undefined
+
+}
+
+export interface searchManufacturerProps {
+    manufacturer : string;
+    setManufacturer:(manufacturer:string) => void;
+}
+
+export interface CarProps{
+city_mpg:number; 
+class:string;
+combination_mpg:number;
+cylinders:number;
+displacement:number;
+drive:string;
+fuel_type:string;
+highway_mpg:number;
+make:string;
+model:string;
+transmission:string;
+year:number;
+}
